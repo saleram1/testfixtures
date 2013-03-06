54685908e9b68267d433e557aa3a4af94f504eb0
Michael J Salera <mikesalera@mac.com>
2013-03-05 20:38:45 -0800

    Call the load method - to make test data available to functional tests

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,9 +1,10 @@
 
 class BootStrap {
-    def fixtureLoader
-
+	def fixtureLoader
 
     def init = { servletContext ->
+		// create test data
+		fixtureLoader.load("books/*")
     }
 
     def destroy = {
